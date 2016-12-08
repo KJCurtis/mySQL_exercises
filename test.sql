0b668db5816e007b6b99654ffc289c757f8e2607
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-12-08 10:45:02 -0600

    continuing the where exercise lsn 2

--- test.sql
+++ test.sql
@@ -19,3 +19,34 @@ select first_name, last_name, birth_date from employees where birth_date like "%
 
 select first_name, last_name from employees where last_name like '%q%';
 
+-- 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+
+select first_name, last_name from employees where first_name like 'Irena'
+                                                  or first_name like 'Vidya'
+                                                  or first_name like 'Maya';
+
+-- everybody with those names who is also male — 441 rows.
+
+select first_name, last_name, gender from employees where first_name like 'Irena'
+                                                          or first_name like 'Vidya'
+                                                          or first_name like 'Maya'
+                                                          and gender like 'M';
+
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+
+select first_name , last_name from employees where last_name like 'e%'
+                                                   or last_name like '%e';
+
+-- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+
+select first_name , last_name from employees where last_name like 'e%'
+                                                   and last_name like '%e';
+
+-- Find all employees hired in the 90s and born on Christmas — 362 rows.
+
+SELECT first_name, last_name, birth_date, hire_date from employees where hire_date like '199%'
+                                                                         and birth_date like '%12-24';
+
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+
+SELECT first_name, last_name from employees where last_name like 'Q%' and last_name not LIKE 'qu%';
